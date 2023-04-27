97af011a92bc06d98131ecfaa314df1520f90024
josedbolivarma <josedbolivarmafrontend@gmail.com>
2023-04-27 17:24:47 -0500

    Initial Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './modules/movies/movies.component';
+import { MovieDetailComponent } from './modules/movies/movie-detail/movie-detail.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieDetailComponent,
+    BannerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
